978c8c72b19a9f098c2080cf1b04b4b2bc9311a6
dashashimankaya <dashashiman3@mail.ru>
2026-04-17 13:49:50 +0500

    Add files via upload

---
+++ javascrmoya.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githack.com/dashashimankaya/DashaShimanskaya/refs/heads/main/result_Million%20Dollar%20Depression.jpg",
+
+  "https://raw.githack.com/dashashimankaya/DashaShimanskaya/refs/heads/main/result_PHILARMONIA.jpg",
+
+  "https://raw.githack.com/dashashimankaya/DashaShimanskaya/refs/heads/main/result_PINKFL.jpg",
+
+  "https://raw.githack.com/dashashimankaya/DashaShimanskaya/refs/heads/main/result_phosphor.jpg",
+
+  "https://raw.githack.com/dashashimankaya/DashaShimanskaya/refs/heads/main/result_phuneral.jpg",
+
+  "https://raw.githack.com/dashashimankaya/DashaShimanskaya/refs/heads/main/bfa8755b3504bf2778efbe84baa84fb4%20(1).jpg"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "Million Dollar Depression",
+
+  "PHILARMONIA",
+
+  "PINK PHLOYD",
+
+  "PHOSPHOR",
+
+  "PHUNERAL",
+
+  "10:13"
+
+];
